978a91c2d53aa22ec04b3eb4a4494ba9f6583303
COMPEED\Administrator <mintblue2006@naver.com>
2018-01-16 13:27:45 +0900

    2

---
+++ Healing game project/Assets/NewBehaviourScript2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+	}
+}
